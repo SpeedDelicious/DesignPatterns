96ff55e6b873e59555cd1f58b01d1aaa51ac2187
SpeedDelicious <florian.huber@student.htldornbirn.at>
2021-12-12 22:02:27 +0100

    added inform function and color chance in RectangleActor and created Player class

---
+++ src/at/fh/oo/java/Player.java
@@ -0,0 +1,16 @@
+package at.fh.oo.java;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public class Player implements Actor{
+    @Override
+    public void update(GameContainer gameContainer, int delta) {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}
--- src/at/fh/oo/java/RectangleActor.java
+++ src/at/fh/oo/java/RectangleActor.java
@@ -1,8 +1,11 @@
 package at.fh.oo.java;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 public class RectangleActor extends AbstractActor{
+
+    private Color color;
 
     public RectangleActor(MoveStrategy moveStrategy) {
         super(moveStrategy);
@@ -10,6 +13,12 @@ public class RectangleActor extends AbstractActor{
 
     @Override
     public void render(Graphics graphics) {
+        graphics.setColor(this.color);
         graphics.drawRect(this.moveStrategy.getX(), this.moveStrategy.getY(),20,20);
+        graphics.setColor(Color.white);
+    }
+
+    public void inform(){
+        this.color = Color.orange;
     }
 }
